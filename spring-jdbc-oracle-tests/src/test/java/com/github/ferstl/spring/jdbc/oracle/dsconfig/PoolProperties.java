47b9c2f6ad38360937040d7b9c85bea41b3bec6e
Stefan Ferstl <st.ferstl@gmail.com>
2013-06-22 21:21:31 +0200

    Fix autocommit property

--- spring-jdbc-oracle-tests/src/test/java/com/github/ferstl/spring/jdbc/oracle/dsconfig/PoolProperties.java
+++ spring-jdbc-oracle-tests/src/test/java/com/github/ferstl/spring/jdbc/oracle/dsconfig/PoolProperties.java
@@ -14,13 +14,13 @@ final class PoolProperties {
     props.setProperty("url", env.getProperty("db.url"));
     props.setProperty("username", env.getProperty("db.username"));
     props.setProperty("password", env.getProperty("db.password"));
+    props.setProperty("defaultAutoCommit ", env.getProperty("db.defaultAutoCommit"));
+
     props.setProperty("driverClassName", env.getProperty("driverClassName"));
     props.setProperty("maxActive", env.getProperty("maxActive"));
     props.setProperty("maxIdle", env.getProperty("maxIdle"));
     props.setProperty("minIdle", env.getProperty("minIdle"));
     props.setProperty("initialSize", env.getProperty("initialSize"));
-
-    props.setProperty("db.defaultAutoCommit ", env.getProperty("defaultAutoCommit"));
 
     return props;
   }
