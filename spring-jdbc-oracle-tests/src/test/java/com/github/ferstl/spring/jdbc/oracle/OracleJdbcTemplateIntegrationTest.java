19f99ed4230e89aa9149ca3a6581df2e359cad84
Stefan Ferstl <st.ferstl@gmail.com>
2013-06-22 09:33:10 +0200

    Add @IfProfileValue

--- spring-jdbc-oracle-tests/src/test/java/com/github/ferstl/spring/jdbc/oracle/OracleJdbcTemplateIntegrationTest.java
+++ spring-jdbc-oracle-tests/src/test/java/com/github/ferstl/spring/jdbc/oracle/OracleJdbcTemplateIntegrationTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.annotation.IfProfileValue;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -19,6 +20,7 @@ import static org.junit.Assert.assertEquals;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = DatabaseConfiguration.class)
 @TransactionConfiguration
+@IfProfileValue(name = "testgroup", value="integration")
 public class OracleJdbcTemplateIntegrationTest {
 
   @Autowired
