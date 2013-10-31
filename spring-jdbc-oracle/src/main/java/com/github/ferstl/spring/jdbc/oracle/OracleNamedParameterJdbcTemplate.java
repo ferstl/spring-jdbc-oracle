96c07018217ebc2a5cec53b93d7fb00ae28cde22
Stefan Ferstl <st.ferstl@gmail.com>
2013-10-31 06:08:15 +0100

    Fix indentation

--- spring-jdbc-oracle/src/main/java/com/github/ferstl/spring/jdbc/oracle/OracleNamedParameterJdbcTemplate.java
+++ spring-jdbc-oracle/src/main/java/com/github/ferstl/spring/jdbc/oracle/OracleNamedParameterJdbcTemplate.java
@@ -34,175 +34,175 @@ import org.springframework.jdbc.core.namedparam.SqlParameterSource;
  */
 public final class OracleNamedParameterJdbcTemplate extends NamedParameterJdbcTemplate {
 
-    /**
-     * Create a new OracleNamedParameterJdbcTemplate for the given classic
-     * Spring {@link org.springframework.jdbc.core.JdbcTemplate}.
-     * @param classicJdbcTemplate the classic Spring JdbcTemplate to wrap
-     */
-    public OracleNamedParameterJdbcTemplate(JdbcOperations classicJdbcTemplate) {
-        super(classicJdbcTemplate);
+  /**
+   * Create a new OracleNamedParameterJdbcTemplate for the given classic
+   * Spring {@link org.springframework.jdbc.core.JdbcTemplate}.
+   * @param classicJdbcTemplate the classic Spring JdbcTemplate to wrap
+   */
+  public OracleNamedParameterJdbcTemplate(JdbcOperations classicJdbcTemplate) {
+    super(classicJdbcTemplate);
+  }
+
+  @Override
+  protected PreparedStatementCreator getPreparedStatementCreator(String sql, SqlParameterSource parameterSource) {
+    List<String> parameterNames = findParameterNames(sql);
+    List<SqlParameter> parameters = new ArrayList<>(parameterNames.size());
+    Object[] values = new Object[parameterNames.size()];
+
+    List<Integer> collectionIndices = getCollectionIndices(parameterNames, parameterSource);
+    if (!collectionIndices.isEmpty()) {
+      values = flatten(values);
+      sql = expandCollectionPlaceholders(sql, collectionIndices, parameterNames, parameterSource);
     }
 
-    @Override
-    protected PreparedStatementCreator getPreparedStatementCreator(String sql, SqlParameterSource parameterSource) {
-        List<String> parameterNames = findParameterNames(sql);
-        List<SqlParameter> parameters = new ArrayList<>(parameterNames.size());
-        Object[] values = new Object[parameterNames.size()];
-
-        List<Integer> collectionIndices = getCollectionIndices(parameterNames, parameterSource);
-        if (!collectionIndices.isEmpty()) {
-            values = flatten(values);
-            sql = expandCollectionPlaceholders(sql, collectionIndices, parameterNames, parameterSource);
-        }
-
-        int i = 0;
-        for (String parameterName : parameterNames) {
-            int sqlType = parameterSource.getSqlType(parameterName);
-            String typeName = parameterSource.getTypeName(parameterName);
-            SqlParameter parameter = new SqlParameter(sqlType, typeName);
-            parameters.add(parameter);
-            values[i++] = parameterSource.getValue(parameterName);
-        }
-        PreparedStatementCreatorFactory pscf = new PreparedStatementCreatorFactory(sql, parameters);
-        return pscf.newPreparedStatementCreator(values);
+    int i = 0;
+    for (String parameterName : parameterNames) {
+      int sqlType = parameterSource.getSqlType(parameterName);
+      String typeName = parameterSource.getTypeName(parameterName);
+      SqlParameter parameter = new SqlParameter(sqlType, typeName);
+      parameters.add(parameter);
+      values[i++] = parameterSource.getValue(parameterName);
     }
+    PreparedStatementCreatorFactory pscf = new PreparedStatementCreatorFactory(sql, parameters);
+    return pscf.newPreparedStatementCreator(values);
+  }
 
-    private String expandCollectionPlaceholders(String sql, List<Integer> collectionIndices, List<String> parameterNames, SqlParameterSource parameterSource) {
-        StringBuilder buffer = new StringBuilder((int) (sql.length() * 1.1));
-        Iterator<Integer> collectionIterator = collectionIndices.iterator();
-        int collectionIndex = collectionIterator.next();
+  private String expandCollectionPlaceholders(String sql, List<Integer> collectionIndices, List<String> parameterNames, SqlParameterSource parameterSource) {
+    StringBuilder buffer = new StringBuilder((int) (sql.length() * 1.1));
+    Iterator<Integer> collectionIterator = collectionIndices.iterator();
+    int collectionIndex = collectionIterator.next();
 
-        int startIndex = 0;
-        int parameterIndex = 0;
-        while (startIndex >= 0) {
-            int index = sql.indexOf(':', startIndex);
-            if (index >= 0) {
-                int endIndex = sql.length();
-                for (int i = index + 1; i < sql.length(); ++i) {
-                    char c = sql.charAt(i);
-                    if (isTerminator(c)) {
-                        endIndex = i;
-                        break;
-                    }
-                }
-
-                if (parameterIndex == collectionIndex) {
-                    buffer.append(sql, startIndex, index);
-                    String parameterName = parameterNames.get(parameterIndex);
-                    Collection<?> value = (Collection<?>) parameterSource.getValue(parameterName);
-                    addPlaceholders(buffer, value.size());
-                    if (collectionIterator.hasNext()) {
-                        collectionIndex = collectionIterator.next();
-                    }
-
-                } else {
-                    buffer.append(sql, startIndex, endIndex);
-                }
-
-                if (endIndex == sql.length()) {
-                    break;
-                } else if (parameterIndex == collectionIndex && !collectionIterator.hasNext()) {
-                    buffer.append(sql, endIndex, sql.length());
-                    break;
-                } else {
-                    startIndex = endIndex;
-                    parameterIndex += 1;
-                }
-            } else {
-                break;
-            }
+    int startIndex = 0;
+    int parameterIndex = 0;
+    while (startIndex >= 0) {
+      int index = sql.indexOf(':', startIndex);
+      if (index >= 0) {
+        int endIndex = sql.length();
+        for (int i = index + 1; i < sql.length(); ++i) {
+          char c = sql.charAt(i);
+          if (isTerminator(c)) {
+            endIndex = i;
+            break;
+          }
         }
 
-        return buffer.toString();
-    }
+        if (parameterIndex == collectionIndex) {
+          buffer.append(sql, startIndex, index);
+          String parameterName = parameterNames.get(parameterIndex);
+          Collection<?> value = (Collection<?>) parameterSource.getValue(parameterName);
+          addPlaceholders(buffer, value.size());
+          if (collectionIterator.hasNext()) {
+            collectionIndex = collectionIterator.next();
+          }
 
-    private void addPlaceholders(StringBuilder buffer, int count) {
-        for (int i = 0; i < count; ++i) {
-            if (i > 0) {
-                buffer.append(',');
-            }
-            buffer.append('?');
-        }
-    }
-
-    private Object[] flatten(Object[] values) {
-        int size = 0;
-        for (Object value : values) {
-            if (value instanceof Collection) {
-                size += ((Collection<?>) value).size();
-            } else {
-                size += 1;
-            }
-        }
-        Object[] flat = new Object[size];
-        int i = 0;
-        for (Object value : values) {
-            if (value instanceof Collection) {
-                for (Object inner : (Collection<?>) value) {
-                    flat[i++] = inner;
-                }
-            } else {
-                flat[i++] = value;
-            }
-        }
-
-        return flat;
-    }
-
-    private List<Integer> getCollectionIndices(List<String> parameterNames, SqlParameterSource parameterSource) {
-        List<Integer> indices = null;
-        for (int i = 0; i < parameterNames.size(); ++i) {
-            String parameterName = parameterNames.get(i);
-            Object value = parameterSource.getValue(parameterName);
-            if (value instanceof Collection) {
-                if (indices == null) {
-                    indices = new ArrayList<>(2);
-                }
-                indices.add(i);
-            }
-        }
-
-        if (indices != null) {
-            return indices;
         } else {
-            return Collections.emptyList();
+          buffer.append(sql, startIndex, endIndex);
         }
+
+        if (endIndex == sql.length()) {
+          break;
+        } else if (parameterIndex == collectionIndex && !collectionIterator.hasNext()) {
+          buffer.append(sql, endIndex, sql.length());
+          break;
+        } else {
+          startIndex = endIndex;
+          parameterIndex += 1;
+        }
+      } else {
+        break;
+      }
     }
 
-    private List<String> findParameterNames(String sql) {
-        // FIXME will fail for comments eg
-        // SELECT 1 FROM dual WHERE 1 = 1 -- and 2 = :one
-        // FIXME will fail for stirng litersl eg
-        // SELECT 1 FROM dual WHERE 2 != "2 = :one"
-        List<String> names = new ArrayList<>();
-        int startIndex = 0;
-        while (startIndex >= 0) {
-            int index = sql.indexOf(':', startIndex);
-            if (index >= 0) {
-                int endIndex = sql.length();
-                for (int i = index + 1; i < sql.length(); ++i) {
-                    char c = sql.charAt(i);
-                    if (isTerminator(c)) {
-                        endIndex = i;
-                        break;
-                    }
-                }
-                if (endIndex < sql.length()) {
-                    names.add(sql.substring(index + 1, endIndex));
-                    startIndex = endIndex + 1;
-                } else {
-                    names.add(sql.substring(index + 1));
-                    break;
-                }
-            } else {
-                break;
-            }
+    return buffer.toString();
+  }
+
+  private void addPlaceholders(StringBuilder buffer, int count) {
+    for (int i = 0; i < count; ++i) {
+      if (i > 0) {
+        buffer.append(',');
+      }
+      buffer.append('?');
+    }
+  }
+
+  private Object[] flatten(Object[] values) {
+    int size = 0;
+    for (Object value : values) {
+      if (value instanceof Collection) {
+        size += ((Collection<?>) value).size();
+      } else {
+        size += 1;
+      }
+    }
+    Object[] flat = new Object[size];
+    int i = 0;
+    for (Object value : values) {
+      if (value instanceof Collection) {
+        for (Object inner : (Collection<?>) value) {
+          flat[i++] = inner;
         }
-        return names;
+      } else {
+        flat[i++] = value;
+      }
     }
 
-    private boolean isTerminator(char c) {
-        return c == ')' || Character.isWhitespace(c);
+    return flat;
+  }
+
+  private List<Integer> getCollectionIndices(List<String> parameterNames, SqlParameterSource parameterSource) {
+    List<Integer> indices = null;
+    for (int i = 0; i < parameterNames.size(); ++i) {
+      String parameterName = parameterNames.get(i);
+      Object value = parameterSource.getValue(parameterName);
+      if (value instanceof Collection) {
+        if (indices == null) {
+          indices = new ArrayList<>(2);
+        }
+        indices.add(i);
+      }
     }
+
+    if (indices != null) {
+      return indices;
+    } else {
+      return Collections.emptyList();
+    }
+  }
+
+  private List<String> findParameterNames(String sql) {
+    // FIXME will fail for comments eg
+    // SELECT 1 FROM dual WHERE 1 = 1 -- and 2 = :one
+    // FIXME will fail for stirng litersl eg
+    // SELECT 1 FROM dual WHERE 2 != "2 = :one"
+    List<String> names = new ArrayList<>();
+    int startIndex = 0;
+    while (startIndex >= 0) {
+      int index = sql.indexOf(':', startIndex);
+      if (index >= 0) {
+        int endIndex = sql.length();
+        for (int i = index + 1; i < sql.length(); ++i) {
+          char c = sql.charAt(i);
+          if (isTerminator(c)) {
+            endIndex = i;
+            break;
+          }
+        }
+        if (endIndex < sql.length()) {
+          names.add(sql.substring(index + 1, endIndex));
+          startIndex = endIndex + 1;
+        } else {
+          names.add(sql.substring(index + 1));
+          break;
+        }
+      } else {
+        break;
+      }
+    }
+    return names;
+  }
+
+  private boolean isTerminator(char c) {
+    return c == ')' || Character.isWhitespace(c);
+  }
 
 }
--- spring-jdbc-oracle/src/test/java/com/github/ferstl/spring/jdbc/oracle/OracleNamedParameterJdbcTemplateTest.java
+++ spring-jdbc-oracle/src/test/java/com/github/ferstl/spring/jdbc/oracle/OracleNamedParameterJdbcTemplateTest.java
@@ -15,10 +15,6 @@
  */
 package com.github.ferstl.spring.jdbc.oracle;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -32,176 +28,180 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 /**
  * JUnit tests for {@link OracleNamedParameterJdbcTemplate}.
  */
 public class OracleNamedParameterJdbcTemplateTest {
 
-    private OracleNamedParameterJdbcTemplate namedJdbcTemplate;
+  private OracleNamedParameterJdbcTemplate namedJdbcTemplate;
 
-    @Before
-    public void setUp() {
-        JdbcTemplate jdbcTemplate = mock(JdbcTemplate.class);
-        this.namedJdbcTemplate = new OracleNamedParameterJdbcTemplate(jdbcTemplate);
-    }
+  @Before
+  public void setUp() {
+    JdbcTemplate jdbcTemplate = mock(JdbcTemplate.class);
+    this.namedJdbcTemplate = new OracleNamedParameterJdbcTemplate(jdbcTemplate);
+  }
 
-    @Test
-    public void endingNoSpace() throws SQLException {
-        Map<String, Object> map = new HashMap<>(3);
-        map.put("one", 1);
-        map.put("ten", 10);
-        map.put("twenty", 20);
-        String sql = "SELECT 1 FROM dual WHERE 1 = :ten or 20 = :twenty";
-        PreparedStatementCreator preparedStatementCreator = this.namedJdbcTemplate.getPreparedStatementCreator(
-                sql, new MapSqlParameterSource(map));
+  @Test
+  public void endingNoSpace() throws SQLException {
+    Map<String, Object> map = new HashMap<>(3);
+    map.put("one", 1);
+    map.put("ten", 10);
+    map.put("twenty", 20);
+    String sql = "SELECT 1 FROM dual WHERE 1 = :ten or 20 = :twenty";
+    PreparedStatementCreator preparedStatementCreator = this.namedJdbcTemplate.getPreparedStatementCreator(
+        sql, new MapSqlParameterSource(map));
 
-        Connection connection = mock(Connection.class);
-        PreparedStatement preparedStatement = mock(PreparedStatement.class);
+    Connection connection = mock(Connection.class);
+    PreparedStatement preparedStatement = mock(PreparedStatement.class);
 
-        when(connection.prepareStatement(sql)).thenReturn(preparedStatement);
+    when(connection.prepareStatement(sql)).thenReturn(preparedStatement);
 
-        preparedStatementCreator.createPreparedStatement(connection);
+    preparedStatementCreator.createPreparedStatement(connection);
 
-        verify(preparedStatement).setObject(1, 10);
-        verify(preparedStatement).setObject(2, 20);
-    }
+    verify(preparedStatement).setObject(1, 10);
+    verify(preparedStatement).setObject(2, 20);
+  }
 
-    @Test
-    public void endingWithSpace() throws SQLException {
-        Map<String, Object> map = new HashMap<>(3);
-        map.put("one", 1);
-        map.put("ten", 10);
-        map.put("twenty", 20);
-        String sql = "SELECT 1 FROM dual WHERE 10 = :ten or 20 = :twenty ";
-        PreparedStatementCreator preparedStatementCreator = this.namedJdbcTemplate.getPreparedStatementCreator(
-                sql, new MapSqlParameterSource(map));
+  @Test
+  public void endingWithSpace() throws SQLException {
+    Map<String, Object> map = new HashMap<>(3);
+    map.put("one", 1);
+    map.put("ten", 10);
+    map.put("twenty", 20);
+    String sql = "SELECT 1 FROM dual WHERE 10 = :ten or 20 = :twenty ";
+    PreparedStatementCreator preparedStatementCreator = this.namedJdbcTemplate.getPreparedStatementCreator(
+        sql, new MapSqlParameterSource(map));
 
-        Connection connection = mock(Connection.class);
-        PreparedStatement preparedStatement = mock(PreparedStatement.class);
+    Connection connection = mock(Connection.class);
+    PreparedStatement preparedStatement = mock(PreparedStatement.class);
 
-        when(connection.prepareStatement(sql)).thenReturn(preparedStatement);
+    when(connection.prepareStatement(sql)).thenReturn(preparedStatement);
 
-        preparedStatementCreator.createPreparedStatement(connection);
+    preparedStatementCreator.createPreparedStatement(connection);
 
-        verify(preparedStatement).setObject(1, 10);
-        verify(preparedStatement).setObject(2, 20);
-    }
-    
-    @Test
-    public void repetition() throws SQLException {
-      Map<String, Object> map = new HashMap<>(3);
-      map.put("one", 1);
-      map.put("ten", 10);
-      map.put("twenty", 20);
-      String sql = "SELECT 1 FROM dual WHERE 10 = :ten or 0 < :ten ";
-      PreparedStatementCreator preparedStatementCreator = this.namedJdbcTemplate.getPreparedStatementCreator(
-          sql, new MapSqlParameterSource(map));
-      
-      Connection connection = mock(Connection.class);
-      PreparedStatement preparedStatement = mock(PreparedStatement.class);
-      
-      when(connection.prepareStatement(sql)).thenReturn(preparedStatement);
-      
-      preparedStatementCreator.createPreparedStatement(connection);
-      
-      verify(preparedStatement).setObject(1, 10);
-      verify(preparedStatement).setObject(2, 10);
-    }
+    verify(preparedStatement).setObject(1, 10);
+    verify(preparedStatement).setObject(2, 20);
+  }
 
-    @Test
-    public void commonPrefix() throws SQLException {
-        Map<String, Object> map = new HashMap<>(3);
-        map.put("arg", 10);
-        map.put("arg2", 20);
-        String sql = "SELECT 1 FROM dual WHERE 10 = :arg or 20 = :arg2";
-        PreparedStatementCreator preparedStatementCreator = this.namedJdbcTemplate.getPreparedStatementCreator(
-                sql, new MapSqlParameterSource(map));
+  @Test
+  public void repetition() throws SQLException {
+    Map<String, Object> map = new HashMap<>(3);
+    map.put("one", 1);
+    map.put("ten", 10);
+    map.put("twenty", 20);
+    String sql = "SELECT 1 FROM dual WHERE 10 = :ten or 0 < :ten ";
+    PreparedStatementCreator preparedStatementCreator = this.namedJdbcTemplate.getPreparedStatementCreator(
+      sql, new MapSqlParameterSource(map));
 
-        Connection connection = mock(Connection.class);
-        PreparedStatement preparedStatement = mock(PreparedStatement.class);
+    Connection connection = mock(Connection.class);
+    PreparedStatement preparedStatement = mock(PreparedStatement.class);
 
-        when(connection.prepareStatement(sql)).thenReturn(preparedStatement);
+    when(connection.prepareStatement(sql)).thenReturn(preparedStatement);
 
-        preparedStatementCreator.createPreparedStatement(connection);
+    preparedStatementCreator.createPreparedStatement(connection);
 
-        verify(preparedStatement).setObject(1, 10);
-        verify(preparedStatement).setObject(2, 20);
-    }
+    verify(preparedStatement).setObject(1, 10);
+    verify(preparedStatement).setObject(2, 10);
+  }
 
-    @Test
-    public void collection() throws SQLException {
-        Map<String, Object> map = new HashMap<>(3);
-        map.put("ten", 10);
-        map.put("twenty", 20);
-        map.put("collection", Arrays.asList(1, 23, 42));
-        String sql = "SELECT 1 FROM dual WHERE 10 = :ten or 42 in (:collection) or 20 = :twenty";
-        String expectedSql = "SELECT 1 FROM dual WHERE 10 = :ten or 42 in (?,?,?) or 20 = :twenty";
-        PreparedStatementCreator preparedStatementCreator = this.namedJdbcTemplate.getPreparedStatementCreator(
-                sql,
-                new MapSqlParameterSource(map));
+  @Test
+  public void commonPrefix() throws SQLException {
+    Map<String, Object> map = new HashMap<>(3);
+    map.put("arg", 10);
+    map.put("arg2", 20);
+    String sql = "SELECT 1 FROM dual WHERE 10 = :arg or 20 = :arg2";
+    PreparedStatementCreator preparedStatementCreator = this.namedJdbcTemplate.getPreparedStatementCreator(
+        sql, new MapSqlParameterSource(map));
 
-        Connection connection = mock(Connection.class);
-        PreparedStatement preparedStatement = mock(PreparedStatement.class);
+    Connection connection = mock(Connection.class);
+    PreparedStatement preparedStatement = mock(PreparedStatement.class);
 
-        when(connection.prepareStatement(expectedSql)).thenReturn(preparedStatement);
+    when(connection.prepareStatement(sql)).thenReturn(preparedStatement);
 
-        preparedStatementCreator.createPreparedStatement(connection);
+    preparedStatementCreator.createPreparedStatement(connection);
 
-        verify(preparedStatement).setObject(1, 10);
-        verify(preparedStatement).setObject(2, 1);
-        verify(preparedStatement).setObject(3, 23);
-        verify(preparedStatement).setObject(4, 42);
-        verify(preparedStatement).setObject(5, 20);
-    }
+    verify(preparedStatement).setObject(1, 10);
+    verify(preparedStatement).setObject(2, 20);
+  }
 
-    @Test
-    public void endsWithCollection() throws SQLException {
-        Map<String, Object> map = new HashMap<>(3);
-        map.put("ten", 10);
-        map.put("twenty", 20);
-        map.put("collection", Arrays.asList(1, 23, 42));
-        String sql = "SELECT 1 FROM dual WHERE 10 = :ten or 42 in (:collection)";
-        String expectedSql = "SELECT 1 FROM dual WHERE 10 = :ten or 42 in (?,?,?)";
-        PreparedStatementCreator preparedStatementCreator = this.namedJdbcTemplate.getPreparedStatementCreator(
-                sql,
-                new MapSqlParameterSource(map));
+  @Test
+  public void collection() throws SQLException {
+    Map<String, Object> map = new HashMap<>(3);
+    map.put("ten", 10);
+    map.put("twenty", 20);
+    map.put("collection", Arrays.asList(1, 23, 42));
+    String sql = "SELECT 1 FROM dual WHERE 10 = :ten or 42 in (:collection) or 20 = :twenty";
+    String expectedSql = "SELECT 1 FROM dual WHERE 10 = :ten or 42 in (?,?,?) or 20 = :twenty";
+    PreparedStatementCreator preparedStatementCreator = this.namedJdbcTemplate.getPreparedStatementCreator(
+        sql,
+        new MapSqlParameterSource(map));
 
-        Connection connection = mock(Connection.class);
-        PreparedStatement preparedStatement = mock(PreparedStatement.class);
+    Connection connection = mock(Connection.class);
+    PreparedStatement preparedStatement = mock(PreparedStatement.class);
 
-        when(connection.prepareStatement(expectedSql)).thenReturn(preparedStatement);
+    when(connection.prepareStatement(expectedSql)).thenReturn(preparedStatement);
 
-        preparedStatementCreator.createPreparedStatement(connection);
+    preparedStatementCreator.createPreparedStatement(connection);
 
-        verify(preparedStatement).setObject(1, 10);
-        verify(preparedStatement).setObject(2, 1);
-        verify(preparedStatement).setObject(3, 23);
-        verify(preparedStatement).setObject(4, 42);
-    }
+    verify(preparedStatement).setObject(1, 10);
+    verify(preparedStatement).setObject(2, 1);
+    verify(preparedStatement).setObject(3, 23);
+    verify(preparedStatement).setObject(4, 42);
+    verify(preparedStatement).setObject(5, 20);
+  }
 
-    @Test
-    public void endsWithCollectionSpace() throws SQLException {
-        Map<String, Object> map = new HashMap<>(3);
-        map.put("ten", 10);
-        map.put("twenty", 20);
-        map.put("collection", Arrays.asList(1, 23, 42));
-        String sql = "SELECT 1 FROM dual WHERE 10 = :ten or 42 in (:collection) ";
-        String expectedSql = "SELECT 1 FROM dual WHERE 10 = :ten or 42 in (?,?,?) ";
-        PreparedStatementCreator preparedStatementCreator = this.namedJdbcTemplate.getPreparedStatementCreator(
-                sql,
-                new MapSqlParameterSource(map));
+  @Test
+  public void endsWithCollection() throws SQLException {
+    Map<String, Object> map = new HashMap<>(3);
+    map.put("ten", 10);
+    map.put("twenty", 20);
+    map.put("collection", Arrays.asList(1, 23, 42));
+    String sql = "SELECT 1 FROM dual WHERE 10 = :ten or 42 in (:collection)";
+    String expectedSql = "SELECT 1 FROM dual WHERE 10 = :ten or 42 in (?,?,?)";
+    PreparedStatementCreator preparedStatementCreator = this.namedJdbcTemplate.getPreparedStatementCreator(
+        sql,
+        new MapSqlParameterSource(map));
 
-        Connection connection = mock(Connection.class);
-        PreparedStatement preparedStatement = mock(PreparedStatement.class);
+    Connection connection = mock(Connection.class);
+    PreparedStatement preparedStatement = mock(PreparedStatement.class);
 
-        when(connection.prepareStatement(expectedSql)).thenReturn(preparedStatement);
+    when(connection.prepareStatement(expectedSql)).thenReturn(preparedStatement);
 
-        preparedStatementCreator.createPreparedStatement(connection);
+    preparedStatementCreator.createPreparedStatement(connection);
 
-        verify(preparedStatement).setObject(1, 10);
-        verify(preparedStatement).setObject(2, 1);
-        verify(preparedStatement).setObject(3, 23);
-        verify(preparedStatement).setObject(4, 42);
-    }
+    verify(preparedStatement).setObject(1, 10);
+    verify(preparedStatement).setObject(2, 1);
+    verify(preparedStatement).setObject(3, 23);
+    verify(preparedStatement).setObject(4, 42);
+  }
+
+  @Test
+  public void endsWithCollectionSpace() throws SQLException {
+    Map<String, Object> map = new HashMap<>(3);
+    map.put("ten", 10);
+    map.put("twenty", 20);
+    map.put("collection", Arrays.asList(1, 23, 42));
+    String sql = "SELECT 1 FROM dual WHERE 10 = :ten or 42 in (:collection) ";
+    String expectedSql = "SELECT 1 FROM dual WHERE 10 = :ten or 42 in (?,?,?) ";
+    PreparedStatementCreator preparedStatementCreator = this.namedJdbcTemplate.getPreparedStatementCreator(
+        sql,
+        new MapSqlParameterSource(map));
+
+    Connection connection = mock(Connection.class);
+    PreparedStatement preparedStatement = mock(PreparedStatement.class);
+
+    when(connection.prepareStatement(expectedSql)).thenReturn(preparedStatement);
+
+    preparedStatementCreator.createPreparedStatement(connection);
+
+    verify(preparedStatement).setObject(1, 10);
+    verify(preparedStatement).setObject(2, 1);
+    verify(preparedStatement).setObject(3, 23);
+    verify(preparedStatement).setObject(4, 42);
+  }
 
 }
